e4636af87cd3aedc07e0f535f681519fb7330c3c
sammcode <samuel.mcgarry@gmail.com>
2021-02-12 13:50:38 -0500

    Made some changes to PlayRecipeVC

--- RecipeTimer/Screens/PlayRecipeVC.swift
+++ RecipeTimer/Screens/PlayRecipeVC.swift
@@ -123,44 +123,69 @@ class PlayRecipeVC: UIViewController {
     
     //Creates card views for the recipe's ingredients, prep steps, and cooking steps, and adds them to cardViewData array
     func fillCardViewData() {
+        //Declare default amount of recipe steps
         var ingCount = 1
         var prepCount = 1
         var cookCount = 1
+        //Iterate through ingredients in recipe
         for ing in recipe.ingredients {
+            //Declare quantity variable
             let quantity: String!
+            //Check if multiplier has a value, if so apply it to the ingredient's quantity
             if multiplier != nil {
                 quantity = HelpfulFunctions.applyMultiplier(quantity: ing.qnty!, multiplier: multiplier!)
             }else{
                 quantity = ing.qnty
             }
+            //Create a card view for the ingredient and append it to the cardViewData array
             cardViewData.append(WalkthroughCardView(backview: "Ingredient \(ingCount)", title: ing.title, timer: quantity ?? "", notes: ing.notes ?? "", backviewColor: HelpfulFunctions.colorWithGradient(frame: view.bounds, colors: [UIColor(named: "ingredient")!, UIColor(named: "ingredient2")!]), backviewTitleColor: UIColor(named: "ingredient3")!, delegate: self))
+            //Increment the number of ingredients by 1
             ingCount += 1
         }
+        //Iterate through prep steps in recipe
         for prep in recipe.prepSteps {
+            //Create a card view for the prep step and append it to the cardViewData array
             cardViewData.append(WalkthroughCardView(backview: "Prep Step \(prepCount)", title: prep.title, timer: "", notes: prep.notes ?? "", backviewColor: HelpfulFunctions.colorWithGradient(frame: view.bounds, colors: [UIColor(named: "prepstep")!, UIColor(named: "prepstep2")!]), backviewTitleColor: UIColor(named: "prepstep3")!, delegate: self))
+            //Increment the number of cooking steps by 1
             prepCount += 1
         }
+        //Iterate through cooking steps in recipe
         for cook in recipe.cookingSteps {
+            //Create a card view for the cooking step and append it to the cardViewData array
             cardViewData.append(WalkthroughCardView(backview: "Cooking Step \(cookCount)", title: cook.title, timer: cook.timeUltimatum ?? "", notes: "", backviewColor: HelpfulFunctions.colorWithGradient(frame: view.bounds, colors: [UIColor(named: "cookingstep")!, UIColor(named: "cookingstep2")!]), backviewTitleColor: UIColor(named: "cookingstep3")!, delegate: self))
+            //Increment the number of cooking steps by 1
             cookCount += 1
         }
     }
     
+    //Calls functions that help set up the view, sets some basic properties
     func configure() {
+        //Fills cardViewData array
         fillCardViewData()
+        
+        //Sets up the UI elements
         setUpElements()
+        
+        //Adds and action to the "Next" button
         addActionToButton()
+        
+        //Checks if the multiplier has a value, if so it incorporates it into the title
         if multiplier != nil {
             title = recipe.title + " x " + multiplier!
         }else{
             title = recipe.title
         }
+        
+        //Sets the background color for the view
         view.backgroundColor = .systemBackground
+        
+        //Sets the WalkthroughCardView delegate to self
         self.delegate = self
     }
     
+    //Calls the functions that set up constraints for all the UI elements for the view
     func setUpElements(){
-        if recipe.ingredients.count + recipe.prepSteps.count + recipe.cookingSteps.count > 1 { setUpNextButtonConstraints() }
+        setUpNextButtonConstraints()
         setUpCardViewConstraints()
         setUpProgressBarConstraints()
         setUpProgressBars()
@@ -169,12 +194,17 @@ class PlayRecipeVC: UIViewController {
         setUpTapViewConstraints()
     }
     
+    //Sets up the constraints for the tap view
     func setUpTapViewConstraints() {
+        //Adds it to the view as a subview
         view.addSubview(tapView)
+        //Sets background color
         tapView.backgroundColor = UIColor.clear
         
+        //Declares a constant that will represent the height of the view
         let height: CGFloat!
         
+        //Sets the height constant based on the current device type
         if DeviceType.isiPadPro {
             height = ScreenSize.width * 0.8
         }else if DeviceType.isiPad11inch {
@@ -191,6 +221,7 @@ class PlayRecipeVC: UIViewController {
             height = ScreenSize.width * 1.25
         }
         
+        //Activates the constraints
         NSLayoutConstraint.activate([
             tapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             tapView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -198,29 +229,40 @@ class PlayRecipeVC: UIViewController {
             tapView.heightAnchor.constraint(equalToConstant: height)
         ])
         
+        //Declares a tap gesture recognizer that triggers an action if the user taps on the view twice
         let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
         tap.numberOfTapsRequired = 2
         tapView.addGestureRecognizer(tap)
     }
     
+    //Sets up the constraints for the play/pause overlay
     func setUpPlayPauseIndicatorViewConstraints(){
+        //Adds it to the view as a subview
         view.addSubview(playPauseView)
+        //Activates the constraints
         NSLayoutConstraint.activate([
             playPauseView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             playPauseView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             playPauseView.widthAnchor.constraint(equalToConstant: 160),
             playPauseView.heightAnchor.constraint(equalToConstant: 160)
         ])
+        //Sets the isHidden property to true, this is toggled when the user double taps on the screen
         playPauseView.isHidden = true
     }
     
+    //Sets up the constraints for the progress bar stack view
     func setUpProgressBarConstraints() {
+        //Adds it to the view as a subview
         view.addSubview(progressBar)
+        //Makes sure the recipe has ingredients, prep steps, or cooking steps before adding the corresponding progress bars to the stack view
         if recipe.ingredients.count != 0 { progressBar.addArrangedSubview(ingredientProgressBar) }
         if recipe.prepSteps.count != 0 { progressBar.addArrangedSubview(prepstepProgressBar) }
         if recipe.cookingSteps.count != 0 { progressBar.addArrangedSubview(cookingstepProgressBar) }
         
+        //Declares a constant representing the offset from the bottom of the screen
         let constant: CGFloat!
+        
+        //Sets the constant based on the type of device
         if DeviceType.isiPhone8Standard || DeviceType.isiPhone8PlusStandard {
             constant = ScreenSize.height * 0.22
         }else if DeviceType.isiPhoneSE {
@@ -228,6 +270,8 @@ class PlayRecipeVC: UIViewController {
         }else{
             constant = ScreenSize.height * 0.19
         }
+        
+        //Activates the constraints
         NSLayoutConstraint.activate([
             progressBar.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant),
             progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -236,6 +280,7 @@ class PlayRecipeVC: UIViewController {
         ])
     }
     
+    //Sets up the total unit counts and tint colors for the progress bars
     func setUpProgressBars(){
         ingredientProgress = Progress(totalUnitCount: Int64(recipe.ingredients.count))
         prepstepProgress = Progress(totalUnitCount: Int64(recipe.prepSteps.count))
@@ -246,28 +291,44 @@ class PlayRecipeVC: UIViewController {
         cookingstepProgressBar.tintColor = UIColor(named: "cookingstep")
     }
     
+    //Sets up the card view constraints
     func setUpCardViewConstraints(){
+        //Adds it to the view as a subview
         view.addSubview(cardView)
+        
+        //Sets the datasource for the card view
         cardView.dataSource = self
+        
+        //Constrains the cardView to the center of the view
         cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
+    //Sets up the next button constraints
     func setUpNextButtonConstraints(){
+        //Adds it to the view as a subview
         view.addSubview(nextButton)
+        
+        //Declares the constant that represents the height of the button, sets it based on the current device type
         let height: CGFloat = (DeviceType.isiPad || DeviceType.isiPad7thGen || DeviceType.isiPadAir || DeviceType.isiPad11inch || DeviceType.isiPadPro) ? ScreenSize.width * 0.1 : ScreenSize.width * 0.2
+        
+        //Declares the constant that represents the offset from the bottom of the screen
         let constant: CGFloat!
         
+        //Sets the constant based on whether or not the current device is an iPhone SE 1st Gen
         if DeviceType.isiPhoneSE {
             constant = -view.bounds.width/16
         }else {
             constant = -view.bounds.width/15
         }
         
-        nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: constant).isActive = true
-        nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: constant).isActive = true
-        nextButton.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.3).isActive = true
-        nextButton.heightAnchor.constraint(equalToConstant: height).isActive = true
+        //Activates the constraints
+        NSLayoutConstraint.activate([
+            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: constant),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: constant),
+            nextButton.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.3),
+            nextButton.heightAnchor.constraint(equalToConstant: height)
+        ])
     }
     
     func setUpPrevButtonConstraints(){
